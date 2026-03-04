64d2824ff187d827aafb0486379484505a99d958
Kyle Kelley <rgbkrk@gmail.com>
2026-03-04 09:52:30 -0800

    sync(widgets/ipycanvas): update ipycanvas from desktop
    Syncs ipycanvas widget components from desktop app.

--- registry/widgets/ipycanvas/index.ts
+++ registry/widgets/ipycanvas/index.ts
@@ -1,10 +1,3 @@
-/**
- * ipycanvas widget registration.
- *
- * Registers CanvasModel and CanvasManagerModel with the widget registry.
- * Import this module to enable rendering of ipycanvas widgets.
- */
-
 import { registerWidget } from "../widget-registry";
 import { CanvasManagerWidget, CanvasWidget } from "./canvas-widget";
 
